511634832de84c5118e740de563e3608dd29d1c1
Alex Wu <226645041+alex-wu-cn@users.noreply.github.com>
2026-07-03 15:39:09 +0800

    feat:  add multiple robot models and fix joint state publish logic adds visual models and MoveIt configurations for four robotic arms: cr3af, cr5af, cr20af, and nova2s Fix the joint status release logic when the connection is disconnected in the main program and optimize the line break format of the LICENSE file

---
+++ cr20af_moveit/launch/static_virtual_joint_tfs.launch.py
@@ -0,0 +1,8 @@
+﻿from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("cr20af_robot", package_name="cr20af_moveit").to_moveit_configs()
+    return generate_static_virtual_joint_tfs_launch(moveit_config)
+
---
+++ cr5af_moveit/launch/demo.launch.py
@@ -0,0 +1,8 @@
+﻿from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("cr5af_robot", package_name="cr5af_moveit").to_moveit_configs()
+    return generate_demo_launch(moveit_config)
+
